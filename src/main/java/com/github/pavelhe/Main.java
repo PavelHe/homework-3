feb13f79d9f843e5e92bb01323b6605ffdd01dbf
PavelVil <pavel.vavilol.94@mail.ru>
2018-07-11 22:23:39 +0300

    Minor

--- src/main/java/com/github/pavelhe/Main.java
+++ src/main/java/com/github/pavelhe/Main.java
@@ -1,22 +1,24 @@
 package com.github.pavelhe;
 
 import com.github.pavelhe.controller.*;
-import org.springframework.boot.SpringApplication;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.*;
 
 @SpringBootApplication
 @PropertySource("classpath:application.yml")
-public class Main {
+public class Main implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Main.class, args);
-		runQuestionApp();
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
 
-	private static void runQuestionApp() {
-		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Main.class);
-		StudentController controller = context.getBean(StudentController.class);
-		controller.startAnswers();
-	}
+    @Autowired
+    private StudentController controller;
+
+    @Override
+    public void run(String... strings) throws Exception {
+        controller.startAnswers();
+    }
 }
